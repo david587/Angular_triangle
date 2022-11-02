2d1652074ba8c113a00dcbe8152061cf31c93485
Baráth Dávide <davidbarath08@gmail.com>
2022-11-02 09:58:15 +0100

    validation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'triangle';
   //ha egyböl értéket adok akkor nem kell megadni a tipust
-  base !: number;
-  height!: number;
+  base : number = 0;
+  height: number= 0;
   area!: number;
   areaShow = false;
 
   //osztályban nem kell function
 
   calcArea():void{
-    this.area = this.base * this.height / 2;
-    this.base = 0;
-    this.height = 0;
-    this.areaShow= true;
+    if(this.checkValidation()){
+      this.area = this.base * this.height / 2;
+      this.base = 0;
+      this.height = 0;
+      this.areaShow= true;
+    }
+    else{
+      alert("Hiba! Adjon meg adatot")
+    }
+  }
+
+  checkValidation():boolean{
+    let valid = true;
+    if(this.base <= 0){
+      valid= false;
+    }
+    if(this.height <= 0){
+      valid= false;
+    }
+    return valid;
   }
 }
